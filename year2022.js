5c8b0fb60e4704cb1f3b82774ed46aeec83df44c
Van Thai Tran Phong <phongvan1234567899@gmail.com>
2022-07-23 15:59:03 +0700

    fullsource

---
+++ year2022.js
@@ -0,0 +1,9 @@
+const Year2022={
+     wishes:['good health' , 'successful career' , 'happiness' , 'luckiness'],
+     start:function(){
+        for(const wish of this.wishes){
+            console.log('2022 with you' + wish)
+        }
+     }
+}
+Year2022.start()
